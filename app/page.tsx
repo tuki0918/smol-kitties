b8632d0eefb28576cd3e0d670b63d69599fd2ad3
Y.Yamamoto <tuki0918@gmail.com>
2025-01-09 22:58:05 +0900

    ✨ Introduce Main component and update Page to use it

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import Main from "@/components/Main";
+
 export default function Page() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <Main />;
 }
