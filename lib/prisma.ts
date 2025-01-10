909ad66c7358b612f09dba2fddd1549e176ad19c
Y.Yamamoto <tuki0918@gmail.com>
2025-01-10 15:09:11 +0900

    ✨ Introduce Prisma setup with example environment configuration

---
+++ lib/prisma.ts
@@ -0,0 +1,8 @@
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
