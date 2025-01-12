215a7e42264bdb035fc60475dfd4259e1c0befb9
Y.Yamamoto <tuki0918@gmail.com>
2025-01-12 19:35:07 +0900

    💄 Update VideoRepeatButton component with new icons and improved styling

--- components/VideoRepeatButton.tsx
+++ components/VideoRepeatButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { atom, useAtom } from "jotai";
-import { Repeat2 } from "lucide-react";
+import { RefreshCw, RefreshCwOff } from "lucide-react";
 
 export const repeatAtom = atom(true);
 
@@ -14,13 +14,13 @@ export default function VideoRepeatButton() {
   return (
     <button
       type="button"
-      className="w-12 h-12 bg-black bg-opacity-85 rounded-full flex items-center justify-center"
+      className="w-12 h-12 bg-gray-900/50 hover:bg-gray-600/50 rounded-full flex items-center justify-center"
       onClick={toggleRepeat}
     >
       {isRepeatOn ? (
-        <Repeat2 className="w-6 h-6 text-white" />
+        <RefreshCw className="w-6 h-6 text-white" />
       ) : (
-        <Repeat2 className="w-6 h-6 text-gray-600" />
+        <RefreshCwOff className="w-6 h-6 text-white" />
       )}
     </button>
   );
