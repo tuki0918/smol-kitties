f3655ee3e281413a9bfe2f88db2aa50bcc723282
Y.Yamamoto <tuki0918@gmail.com>
2025-01-12 20:23:53 +0900

    💄 Update button structure in DisLikeButton, LikeButton, and ShareButton components

--- components/ShareButton.tsx
+++ components/ShareButton.tsx
@@ -10,11 +10,12 @@ interface ShareButtonProps {
 export default function ShareButton({ video }: ShareButtonProps) {
   // TODO: Implement feature
   return (
-    <div className="relative">
-      <button type="button" className="flex flex-col items-center group">
-        <div className="w-16 h-16 bg-gray-900/50 rounded-full flex items-center justify-center transform transition-all duration-300 hover:scale-110">
-          <Share className="w-8 h-8 text-white" />
-        </div>
+    <div>
+      <button
+        type="button"
+        className="w-16 h-16 bg-gray-900/50 rounded-full flex items-center justify-center transform transition-all duration-300 hover:scale-110"
+      >
+        <Share className="w-8 h-8 text-white" />
       </button>
     </div>
   );
